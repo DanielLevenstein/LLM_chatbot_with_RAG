5b8bd5c68fce3bfd4d8fcdbc3cd001af16123b1e
Daniel Levenstein <danielalevenstein@gmail.com>
2026-06-05 01:31:37 +0000

    Shrinking size of index.faiss by reducing the number of files downloaded and fixing pathing issue in app.py

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 import streamlit as st
-import os
-import sys
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from chatbot.chatbot import ChatBot
 
 
